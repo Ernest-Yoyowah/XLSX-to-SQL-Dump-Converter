4cf9bbffe90e9c5caac936009f9300b210c980a3
ernkeyz <exconzy26@gmail.com>
2023-12-13 20:09:35 +0000

    function convertFile

--- converter.js
+++ converter.js
@@ -29,7 +29,7 @@ function generateSqlDump(rows) {
   const header = rows[0];
   const dataRows = rows.slice(1);
 
-  const tableName = "applicants";
+  const tableName = "people";
 
   const columns = header.map((col) => `\`${col}\``).join(", ");
   const values = dataRows
